365bedd904a6d1f251159c7eb432e0e51a18d9fd
Gabriel Silva <gabrielmdhtt@gmail.com>
2023-07-26 09:58:34 -0400

    config: setting up eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'github.com'
+    ]
+  },
+
+  /**
+   *
+   * @param {import('webpack').Configuration} config
+   */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
---
+++ src/app/providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+import { ReactNode, useState } from 'react'
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {/* <SessionProvider> */}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      {/* </SessionProvider> */}
+    </ThemeProvider>
+  )
+}
