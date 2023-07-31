6f21a03cd3fb56abf3a0dac0ec9821eec2cd0d1e
Gabriel Silva <gabrielmdhtt@gmail.com>
2023-07-31 16:52:04 -0400

    feat(WASH-0001): added back button

--- src/app/(app)/users/[id]/subscription/page.tsx
+++ src/app/(app)/users/[id]/subscription/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next'
 import { SubscriptionForm } from '../tabs/subscriptions/subscription-form'
 import { prisma } from '@/lib/db'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
 
 interface CreateSubscriptionProps {
   params: { id: string }
@@ -27,9 +30,17 @@ export default async function CreateSubscription({
 
   return (
     <>
-      <h2 className="truncate text-3xl font-bold tracking-tight">
-        Subscription
-      </h2>
+      <div className="flex justify-between">
+        <h2 className="truncate text-3xl font-bold tracking-tight">
+          Subscription
+        </h2>
+        <Link href={`/users/${userId}`}>
+          <Button variant="link" size="icon">
+            <ArrowLeft className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <span className="sr-only">Go back</span>
+          </Button>
+        </Link>
+      </div>
 
       <SubscriptionForm userId={userId} vehicles={vehicles} />
     </>
--- src/app/(app)/users/[id]/vehicle/[vehicleId]/page.tsx
+++ src/app/(app)/users/[id]/vehicle/[vehicleId]/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next'
 import { VehicleForm } from '../../tabs/subscriptions/vehicle-form'
 import { prisma } from '@/lib/db'
+import Link from 'next/link'
+import { ArrowLeft } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 interface UpdateVehicleProps {
   params: { id: string; vehicleId: string }
@@ -25,7 +28,15 @@ export default async function UpdateVehicle({ params }: UpdateVehicleProps) {
 
   return (
     <>
-      <h2 className="truncate text-3xl font-bold tracking-tight">Vehicle</h2>
+      <div className="flex justify-between">
+        <h2 className="truncate text-3xl font-bold tracking-tight">Vehicle</h2>
+        <Link href={`/users/${userId}`}>
+          <Button variant="link" size="icon">
+            <ArrowLeft className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <span className="sr-only">Go back</span>
+          </Button>
+        </Link>
+      </div>
 
       <VehicleForm userId={userId} vehicle={vehicle ?? undefined} />
     </>
