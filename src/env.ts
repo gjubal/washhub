708f2514a9361aca6b80f4b76b28b4f06cf5fdd5
Gabriel Silva <gabrielmdhtt@gmail.com>
2023-07-31 17:39:45 -0400

    config(WASH-0001): edited env types

--- src/env.ts
+++ src/env.ts
@@ -12,9 +12,6 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
   },
-  client: {
-    NEXT_PUBLIC_VERCEL_URL: z.string().url().min(1),
-  },
   shared: {
     NODE_ENV: nodeEnv,
     VERCEL_ENV: z
@@ -22,7 +19,6 @@ export const env = createEnv({
       .default('development'),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
     NODE_ENV: process.env.NODE_ENV,
     VERCEL_ENV: process.env.VERCEL_ENV,
   },
